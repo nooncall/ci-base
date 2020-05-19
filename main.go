443a8c0486bb75b06aeb5b36eae551e14abacad7
eastfisher <eastfisher72@gmail.com>
2020-05-19 19:48:03 +0800

    update main

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	
+
 	"github.com/nooncall/ci-base/pkg/utils"
 )
 
 func main() {
 	fmt.Println(utils.FooWord("hello world!"))
+	fmt.Println("do something bad!")
 }
